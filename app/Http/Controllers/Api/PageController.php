142b5d993bf78e53ddcce9b168493a206a3aabee
carlo-ddm <carloduminuco90@gmail.com>
2022-07-14 11:14:02 +0200

    creo API che genera json contenente tutti post

---
+++ app/Http/Controllers/Api/PageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Post;
+
+class PageController extends Controller
+{
+    public function index(){
+        $post = Post::all();
+        return response()->json(compact('post'));
+    }
+}
