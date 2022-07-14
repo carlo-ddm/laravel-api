e6a97608a5812833e72cc2bcbeea491dfb48d116
carlo-ddm <carloduminuco90@gmail.com>
2022-07-14 11:56:55 +0200

    separate i js in front e admin
    aggiornare di conseguenza i percorsi in webpack.mix e nelle view

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{asset('css/front/style.css')}}">
+    <script src="{{asset('js/front.js')}}"></script>
     <title>BoolPress</title>
 </head>
 <body>
